56016fb1818bee47346d776d06d188bbc4bd3786
Ajai Karthikeshan <ajai.karthikeshancs@gmail.com>
2025-01-30 12:52:28 -0800

    Created initial React App

---
+++ lettersOfHope/src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css';
+import HeroSection from './components/HeroSection';
+import AboutSection from './components/AboutSection';
+import ORICESection from './components/ORICESection';
+import LetterCarousel from './components/LetterCarousel';
+
+function App() {
+  return (
+    <>
+      <HeroSection />
+      <AboutSection />
+      <ORICESection />
+      <LetterCarousel />
+    </>
+  );
+}
+
+export default App;
